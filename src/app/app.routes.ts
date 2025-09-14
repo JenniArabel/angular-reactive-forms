7b6a8734dc127adb9e96577c91035a2e5a69e236
Jennifer Arabel <jennifer.arabel@gmail.com>
2025-09-14 11:50:19 -0300

    Diferencias en importacion con lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,13 @@ export const routes: Routes = [
     path: 'reactive',
     loadChildren: () =>
       import('./reactive/reactive.routes').then((m) => m.reactiveRoutes),
+    // Ruta perezosa (lazy loading)
+    // m de módulo
   },
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.routes'),
+    // En este caso no es necesario el .then porque auth.routes tiene una exportación por defecto.
   },
   {
     path: 'country',
