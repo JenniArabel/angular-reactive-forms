c1d4b8f92d9bc6f270772d7f5bd1c80723608d6e
Jennifer Arabel <jennifer.arabel@gmail.com>
2025-09-25 07:40:27 -0300

    Reactive Menu: pasa cada ruta de Angular a ser un elemento de un Menú usando filter y map.

--- src/app/shared/components/side-menu/side-menu.component.ts
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -8,6 +8,8 @@ interface MenuItem {
 }
 
 const reactiveItems = reactiveRoutes[0].children ?? [];
+// reactiveItems es un array de rutas hijas, si no hay rutas hijas, será un array vacío.
+// reactiveRoutes[0] es la primera ruta del array de rutas reactivas.
 
 @Component({
   selector: 'app-side-menu',
@@ -21,6 +23,17 @@ export class SideMenuComponent {
       route: `reactive/${item.path}`,
       title: `${item.title}`,
     }));
+  /** ReactiveMenu
+   * Está transformando rutas de Angular
+   * en elementos de menú
+   * para mostrarlos en la navegación lateral de la aplicación.
+   *
+   * reactiveItems: Es un array que contiene las rutas hijas del módulo reactive
+   * filter: Filtra las rutas para excluir aquellas cuyo path es '**' (ruta comodín para páginas no encontradas).
+   * map: Transforma cada ruta restante en un objeto con las propiedades route y title.
+   * route: Construye la ruta completa concatenando 'reactive/' con el path de la ruta.
+   * title: Toma el título definido en la ruta para mostrar en el menú.
+   */
 
   authMenu: MenuItem[] = [
     {
