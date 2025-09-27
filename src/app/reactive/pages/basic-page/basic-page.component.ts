0b68fc5099ac469a73a75747702732dcb077a01e
Jennifer Arabel <jennifer.arabel@gmail.com>
2025-09-27 20:45:50 -0300

    Formulario reactivo: explicación de cada parte, cómo son los valores iniciales, cómo se implementan, qué necesita para detectar los cambios en los input

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,4 @@
-import { JsonPipe } from '@angular/common';
+import { JsonPipe, NgClass } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import {
   FormBuilder,
@@ -11,52 +11,55 @@ import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe, ReactiveFormsModule],
+  imports: [JsonPipe, ReactiveFormsModule, NgClass], // ReactiveFormsModule es necesario para los formularios reactivos, se toma de Angular Forms
+  // Primera vez que usamos un Module: es un paquete o agrupador que tiene pipes, directivas, componentes, servicios, etc
   templateUrl: './basic-page.component.html',
 })
 export class BasicPageComponent {
   private fb = inject(FormBuilder);
   formUtils = FormUtils;
 
+  // Formulario reactivo
   myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required, Validators.minLength(3)]],
-    price: [0, [Validators.required, Validators.min(10)]],
-    inStorage: [0, [Validators.required, Validators.min(0)]],
+    name: ['', [Validators.required, Validators.minLength(3)]], // nombre del producto
+    price: [0, [Validators.required, Validators.min(10)]], // precio del producto
+    inStorage: [0, [Validators.required, Validators.min(0)]], // existencias del producto
   });
 
-  // myForm2 = new FormGroup({
-  //   name: new FormControl('', [], []),
-  //   price: new FormControl(0),
-  //   inStorage: new FormControl(0),
-  // });
+  /**  myForm2 = new FormGroup({
+      name: new FormControl('', [], []), // tipo de dato any, practica no recomendable, por eso se agrega ('', []) y lo mismo en las demas
+      price: new FormControl(0),
+      inStorage: new FormControl(0),
+    });
 
-  // isValidField(fieldName: string): boolean | null {
-  //   return (
-  //     this.myForm.controls[fieldName].errors &&
-  //     this.myForm.controls[fieldName].touched
-  //   );
-  // }
+    isValidField(fieldName: string): boolean | null {
+      return (
+        this.myForm.controls[fieldName].errors &&
+        this.myForm.controls[fieldName].touched
+      );
+    }
 
-  // getFieldError(fieldName: string): string | null {
-  //   if (!this.myForm.controls[fieldName]) return null;
+    getFieldError(fieldName: string): string | null {
+      if (!this.myForm.controls[fieldName]) return null;
 
-  //   const errors = this.myForm.controls[fieldName].errors ?? {};
+      const errors = this.myForm.controls[fieldName].errors ?? {};
 
-  //   for (const key of Object.keys(errors)) {
-  //     switch (key) {
-  //       case 'required':
-  //         return 'Este campo es requerido';
+      for (const key of Object.keys(errors)) {
+        switch (key) {
+          case 'required':
+            return 'Este campo es requerido';
 
-  //       case 'minlength':
-  //         return `Mínimo de ${errors['minlength'].requiredLength} caracteres.`;
+          case 'minlength':
+            return `Mínimo de ${errors['minlength'].requiredLength} caracteres.`;
 
-  //       case 'min':
-  //         return `Valor mínimo de ${errors['min'].min}`;
-  //     }
-  //   }
+          case 'min':
+            return `Valor mínimo de ${errors['min'].min}`;
+        }
+      }
 
-  //   return null;
-  // }
+      return null;
+    }
+  */
 
   onSave() {
     if (this.myForm.invalid) {
