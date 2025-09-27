d872b803e56c42b3e358e2156859c3a8a4b571b3
Jennifer Arabel <jennifer.arabel@gmail.com>
2025-09-27 17:20:26 -0300

    export default en un archivo de rutas

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
-export const authRoutes: Routes = [
+export const authRoutes: Routes = [ // export default en vez de colocarlo al final
   {
     path: '',
     children: [
