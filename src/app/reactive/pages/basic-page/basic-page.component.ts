ed22a769515e20066b404ca06dd3b1830d456cb5
Jennifer Arabel <jennifer.arabel@gmail.com>
2025-09-28 16:52:44 -0300

    Explicar manejo de errores en el formulario y reseteo del mismo

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -16,14 +16,22 @@ import { FormUtils } from '../../../utils/form-utils';
   templateUrl: './basic-page.component.html',
 })
 export class BasicPageComponent {
-  private fb = inject(FormBuilder);
+  private fb = inject(FormBuilder); //Inyeccion de FormBuilder (forma moderna de Forms Reactivos)
   formUtils = FormUtils;
 
   // Formulario reactivo
   myForm: FormGroup = this.fb.group({
+    /** .group reemplaza a new FormGroup
+     Es necesario definir que myForm es de tipo FormGroup para que no de error
+     y se le asigna el valor de fb.group
+    */
+
     name: ['', [Validators.required, Validators.minLength(3)]], // nombre del producto
+    // por lo menos tener al menos 3 caracteres el nombre
     price: [0, [Validators.required, Validators.min(10)]], // precio del producto
+    // por lo menos tener un precio minimo de 10 usd
     inStorage: [0, [Validators.required, Validators.min(0)]], // existencias del producto
+    // por lo menos tener 0 existencias, false si es un numero negativo
   });
 
   /**  myForm2 = new FormGroup({
@@ -33,16 +41,17 @@ export class BasicPageComponent {
     });
 
     isValidField(fieldName: string): boolean | null {
-      return (
+      return ( // Retorno de una condicion
         this.myForm.controls[fieldName].errors &&
-        this.myForm.controls[fieldName].touched
+        this.myForm.controls[fieldName].touched //
       );
     }
 
-    getFieldError(fieldName: string): string | null {
+    getFieldError(fieldName: string): string | null { // Retorno de un string o null
       if (!this.myForm.controls[fieldName]) return null;
 
       const errors = this.myForm.controls[fieldName].errors ?? {};
+      // Si no hay errores, asignar un objeto vacio
 
       for (const key of Object.keys(errors)) {
         switch (key) {
@@ -56,19 +65,21 @@ export class BasicPageComponent {
             return `Valor mínimo de ${errors['min'].min}`;
         }
       }
-
       return null;
     }
   */
 
-  onSave() {
-    if (this.myForm.invalid) {
-      this.myForm.markAllAsTouched();
+    // Cuando se envía el formulario
+  onSave() { // Está llamado desde el HTML (ngSubmit)
+    // Si la persona toca el form (touched)
+    if (this.myForm.invalid) { // Si el formulario no es valido
+      this.myForm.markAllAsTouched(); // Marcar todos los campos como tocados
       return;
     }
 
     console.log(this.myForm.value);
 
+    // Establecer valores por defecto al resetear el formulario luego de enviarlo
     this.myForm.reset({
       price: 0,
       inStorage: 0,
